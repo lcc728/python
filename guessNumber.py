93a4f54d3614fcbe450e64ead68f292959b9a0c3
lcc728 <yhayoo@gmail.com>
2019-10-27 20:57:04 -0400

    Sum using for
    Sum using for

--- guessNumber.py
+++ guessNumber.py
@@ -1,16 +1,16 @@
 #Guess Number use While
 #https://www.george.tw/2019/10/07/python-guess-the-number/
 import random
-number=random.randint(1,100)
+num=random.randint(1,100)
 guessFlag=False
 while guessFlag==False:
       guess=int(input("Enter a number:"))
-      if guess==number:
+      if guess==num:
             print("God job")
             guessFlag=True
-      elif guess > number :
+      elif guess > num:
             print ("Too high")
-      elif guess < number:
+      elif guess < num:
             print ("Too low")
             
         
---
+++ sum_while.py
@@ -0,0 +1,8 @@
+# Sum using while
+#https://www.george.tw/2019/06/10/python-110-加總-for/
+sum=0
+i = 0
+while (i <= 10):
+    sum += i
+    i += 1
+print ("sum :", sum)
