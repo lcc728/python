0d2eac87d0f91579f4e0c40cb83cc5dbe9b3599b
lcc728 <yhayoo@gmail.com>
2020-03-01 19:47:41 -0500

    Update Leibniz.py

--- Leibniz.py
+++ Leibniz.py
@@ -4,16 +4,16 @@ import math
 
 
 count=int(input("Enter a number:"))
-sum=1
+sumLeibniz=1
 denominator=3
 negate=-1
 
 for i in range(0,count):
-    sum += (negate * ( 1 / denominator))
+    sumLeibniz += (negate * ( 1 / denominator))
     denominator += 2
     negate *= -1
 
-total= 4 * sum
+total= 4 * sumLeibniz
 print("We calculated that {:d} times Leibniz was {:.8f}".format(count,total))
 print("The true PI is {:.8f}".format(math.pi))
 print("The diffecent is {:.8f}".format(total - math.pi))
